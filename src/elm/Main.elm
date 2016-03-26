f496dafdca673a528971571d36dc5611349bb7ee
Craig Bilner <craig.bilner@gmail.com>
2016-03-26 21:35:19 +0000

    Add table HTML

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,36 +1,102 @@
 module Main (..) where
 
-import Html exposing (div, button, text)
-import Html.Events exposing (onClick)
+import Html exposing (div, ul, li, text)
+import Html.Attributes exposing (style)
 import StartApp.Simple as StartApp
 
 
-main =
-  StartApp.start { model = model, view = view, update = update }
+port table : List TableTeam
+type alias TableTeam =
+  { team : String
+  , won : Int
+  , drawn : Int
+  , lost : Int
+  , gFor : Int
+  , gAgainst : Int
+  }
 
 
 type alias Model =
-  Int
-
-
-model : Model
-model =
-  0
-
-
-view : Signal.Address Action -> Model -> Html.Html
-view address model =
-  div
-    []
-    [ text "some live score thing" ]
+  { table : List TableTeam
+  }
 
 
 type Action
   = DoSomething
 
 
+model : Model
+model =
+  { table = table
+  }
+
+
+view : Signal.Address Action -> Model -> Html.Html
+view address model =
+  ul
+    [ style listStyle ]
+    (List.map mapTable model.table)
+
+
 update : Action -> Model -> Model
 update action model =
   case action of
     _ ->
-      model + 1
+      model
+
+
+mapTable : TableTeam -> Html.Html
+mapTable { team, won, drawn, lost, gFor, gAgainst } =
+  li
+    [ style listItemStyle ]
+    [ div
+        [ style (cellStyle ++ teamCellStyle) ]
+        [ text team ]
+    , div
+        [ style cellStyle ]
+        [ text <| toString won ]
+    , div
+        [ style cellStyle ]
+        [ text <| toString drawn ]
+    , div
+        [ style cellStyle ]
+        [ text <| toString lost ]
+    , div
+        [ style cellStyle ]
+        [ text <| toString gFor ]
+    , div
+        [ style cellStyle ]
+        [ text <| toString gAgainst ]
+    ]
+
+
+listStyle : List ( String, String )
+listStyle =
+  [ ( "list-style-type", "none" )
+  , ( "margin", "0" )
+  , ( "padding", "0" )
+  , ( "width", "400px" )
+  ]
+
+
+listItemStyle : List ( String, String )
+listItemStyle =
+  [ ( "display", "flex" )
+  , ( "flex-flow", "row" )
+  ]
+
+
+cellStyle : List ( String, String )
+cellStyle =
+  [ ( "flex", "1" )
+  ]
+
+
+teamCellStyle : List ( String, String )
+teamCellStyle =
+  [ ( "flex", "2" )
+  ]
+
+
+main =
+  StartApp.start { model = model, view = view, update = update }
--- src/js/elm-interop.js
+++ src/js/elm-interop.js
@@ -1,1 +1,166 @@
-Elm.fullscreen(Elm.Main)
+const data = {
+  table: [
+    {
+      "team": "Arsenal",
+      "won": 16,
+      "drawn": 7,
+      "lost": 7,
+      "gFor": 48,
+      "gAgainst": 30
+    },
+    {
+      "team": "Aston Villa",
+      "won": 3,
+      "drawn": 7,
+      "lost": 21,
+      "gFor": 22,
+      "gAgainst": 58
+    },
+    {
+      "team": "Bournemouth",
+      "won": 10,
+      "drawn": 8,
+      "lost": 13,
+      "gFor": 38,
+      "gAgainst": 50
+    },
+    {
+      "team": "Chelsea",
+      "won": 10,
+      "drawn": 11,
+      "lost": 9,
+      "gFor": 45,
+      "gAgainst": 41
+    },
+    {
+      "team": "Crystal Palace",
+      "won": 9,
+      "drawn": 6,
+      "lost": 15,
+      "gFor": 32,
+      "gAgainst": 40
+    },
+    {
+      "team": "Everton",
+      "won": 9,
+      "drawn": 11,
+      "lost": 9,
+      "gFor": 51,
+      "gAgainst": 41
+    },
+    {
+      "team": "Leicester",
+      "won": 19,
+      "drawn": 9,
+      "lost": 3,
+      "gFor": 54,
+      "gAgainst": 31
+    },
+    {
+      "team": "Liverpool",
+      "won": 12,
+      "drawn": 8,
+      "lost": 9,
+      "gFor": 45,
+      "gAgainst": 40
+    },
+    {
+      "team": "Man City",
+      "won": 15,
+      "drawn": 6,
+      "lost": 9,
+      "gFor": 52,
+      "gAgainst": 32
+    },
+    {
+      "team": "Man Utd",
+      "won": 14,
+      "drawn": 8,
+      "lost": 8,
+      "gFor": 38,
+      "gAgainst": 27
+    },
+    {
+      "team": "Newcastle",
+      "won": 6,
+      "drawn": 7,
+      "lost": 17,
+      "gFor": 29,
+      "gAgainst": 55
+    },
+    {
+      "team": "Norwich",
+      "won": 7,
+      "drawn": 7,
+      "lost": 17,
+      "gFor": 32,
+      "gAgainst": 54
+    },
+    {
+      "team": "Southampton",
+      "won": 13,
+      "drawn": 8,
+      "lost": 10,
+      "gFor": 41,
+      "gAgainst": 32
+    },
+    {
+      "team": "Stoke",
+      "won": 13,
+      "drawn": 7,
+      "lost": 11,
+      "gFor": 34,
+      "gAgainst": 37
+    },
+    {
+      "team": "Sunderland",
+      "won": 6,
+      "drawn": 8,
+      "lost": 16,
+      "gFor": 36,
+      "gAgainst": 55
+    },
+    {
+      "team": "Swansea",
+      "won": 9,
+      "drawn": 9,
+      "lost": 13,
+      "gFor": 31,
+      "gAgainst": 40
+    },
+    {
+      "team": "Tottenham",
+      "won": 17,
+      "drawn": 10,
+      "lost": 4,
+      "gFor": 56,
+      "gAgainst": 24
+    },
+    {
+      "team": "WatgFord",
+      "won": 10,
+      "drawn": 7,
+      "lost": 13,
+      "gFor": 30,
+      "gAgainst": 32
+    },
+    {
+      "team": "West Brom",
+      "won": 10,
+      "drawn": 9,
+      "lost": 11,
+      "gFor": 30,
+      "gAgainst": 37
+    },
+    {
+      "team": "West Ham",
+      "won": 13,
+      "drawn": 11,
+      "lost": 6,
+      "gFor": 47,
+      "gAgainst": 35
+    }
+  ]
+};
+
+Elm.fullscreen(Elm.Main, data);
