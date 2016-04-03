8ae6814337e740cc743f8b92d498c3e4452efc9a
Craig Bilner <craig.bilner@gmail.com>
2016-04-04 00:17:20 +0100

    Add more randomness

--- src/elm/games/Update.elm
+++ src/elm/games/Update.elm
@@ -19,8 +19,8 @@ updateTeam random team =
     { team | score = newScore }
 
 
-updateGame : FixturesModel.Fixture -> Int -> FixturesModel.Fixture
-updateGame fixture random =
+updateGame : FixturesModel.Fixture -> ( Int, Int ) -> FixturesModel.Fixture
+updateGame fixture ( r1, r2 ) =
   let
     homeTeam =
       fst fixture.teams
@@ -29,10 +29,10 @@ updateGame fixture random =
       snd fixture.teams
 
     updatedHomeTeam =
-      updateTeam random homeTeam
+      updateTeam r1 homeTeam
 
     updatedAwayTeam =
-      updateTeam random awayTeam
+      updateTeam r2 awayTeam
   in
     { fixture | teams = ( updatedHomeTeam, updatedAwayTeam ) }
 
@@ -52,12 +52,22 @@ update model isPlaying =
           generator =
             Random.list (List.length model.fixtures) (Random.int 0 100)
 
-          seed =
+          seed1 =
             Random.initialSeed model.seedInt
 
-          randomList =
-            Random.generate generator seed
+          seed2 =
+            Random.initialSeed (model.seedInt + 1)
+
+          randomList1 =
+            Random.generate generator seed1
               |> fst
+
+          randomList2 =
+            Random.generate generator seed2
+              |> fst
+
+          randomList =
+            List.map2 (,) randomList1 randomList2
         in
           List.map2 updateGame model.fixtures randomList
       else
@@ -66,4 +76,5 @@ update model isPlaying =
     { model
       | gameTime = newTime
       , fixtures = newGames
+      , seedInt = model.seedInt + 1
     }
