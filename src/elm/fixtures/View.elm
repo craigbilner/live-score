7e2c3b4e5b5990f76c9ccfcd35694af5655fb50f
Craig Bilner <craig.bilner@gmail.com>
2016-04-11 23:52:37 +0100

    Flesh out flow

--- src/elm/fixtures/View.elm
+++ src/elm/fixtures/View.elm
@@ -1,6 +1,7 @@
 module Fixtures.View (..) where
 
 import Html exposing (div, button, text)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 import Fixtures.Model as FixturesModel
 import Update
@@ -11,12 +12,31 @@ view : Signal.Address Update.Action -> FixturesModel.Model -> Html.Html
 view address model =
   div
     []
-    [ button
-        [ onClick address Update.GenerateFixtures ]
-        [ text "create fixtures" ]
-    , button
-        [ onClick address Update.Start ]
-        [ text "kick off" ]
+    [ div
+        [ class "fixtures_buttons" ]
+        [ div
+            [ class "fixtures_step fixtures_step-1" ]
+            [ div
+                [ class "fixtures_step_text fixtures_step_text-1" ]
+                [ text "1" ]
+            , button
+                [ onClick address Update.GenerateFixtures
+                , class "fixtures_button fixtures_step-1_button"
+                ]
+                [ text "create fixtures" ]
+            ]
+        , div
+            [ class "fixtures_step fixtures_step-2" ]
+            [ div
+                [ class "fixtures_step_text fixtures_step_text-2" ]
+                [ text "2" ]
+            , button
+                [ onClick address Update.Start
+                , class "fixtures_button fixtures_step-2_button"
+                ]
+                [ text "kick off" ]
+            ]
+        ]
     , div
         []
         [ text <| toString model.gameTime ]
